5a1f89b8a358adfbe1d891cc8cdd864ccea2de24
ellisju37073 <67281488+ellisju37073@users.noreply.github.com>
2022-02-16 08:09:50 -0800

    new

--- states.py
+++ states.py
@@ -9,11 +9,11 @@ dbname="states"
 engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
             .format(host=hostname, db=dbname, user=uname, pw=pwd))
 
-tables = pandas.read_csv(r"C:\Users\ellis\Documents\Populationstate.csv")
+tables = pandas.read_csv(r"Populationstate.csv")
 
 
 
 connection=engine.connect()
-tables.to_sql('States',con = engine, if_exists = 'replace')
+tables.to_sql('states',con = engine, if_exists = 'replace')
 connection.close()
 print(tables)
