83cc0454c576bb87804a3d626baf14e90cfd95a6
tsabin2023 <148308137+tsabin2023@users.noreply.github.com>
2024-05-11 15:45:40 -0700

    Zip code search outputted 3 rows, check grading rubric.

--- states/rest_web/rest_web.py
+++ states/rest_web/rest_web.py
@@ -24,46 +24,46 @@ conn = mysql.connector.connect(user='root', password='',
                                buffered = True)
 cursor = conn.cursor()
 
-#Search state database
-@app.route('/searchSTATE/<searchState>')
-def searchstate(searchState):
+#Search zip database
+@app.route('/searchZIP/<searchZip>')
+def searchzip(searchZip):
     # Get data from database
-    cursor.execute("SELECT * FROM `states` WHERE State=%s", [searchState])
+    cursor.execute("SELECT * FROM `zipcodes` WHERE Zip=%s", [searchZip])
     test = cursor.rowcount
     if test != 1:
-        return searchState + " was not found"
+        return searchZip + " was not found"
     else:
         searched = cursor.fetchall()
         return 'Success! Here you go: %s' % searched
 
 #update state database population for a specified state
-@app.route('/updatestatepop/<updateSTATE> <updatePOP>')
-def updatestatepop(updateSTATE, updatePOP):
-    cursor.execute("SELECT * FROM `states` WHERE State=%s", [updateSTATE])
+@app.route('/updateszippop/<updateZIP> <updatePOP>')
+def updateszippop(updateZIP, updatePOP):
+    cursor.execute("SELECT * FROM `states` WHERE State=%s", [updateZIP])
     test = cursor.rowcount
     if test != 1:
-        return updateSTATE + " was not found"
+        return updateZIP + " was not found"
     else:
-        cursor.execute("UPDATE `states` SET Pop = %s WHERE State= %s;", [updatePOP,updateSTATE])
-        cursor.execute("SELECT * FROM `states` WHERE State=%s and Pop=%s", [updateSTATE,updatePOP])
+        cursor.execute("UPDATE `states` SET Pop = %s WHERE State= %s;", [updatePOP,updateZIP])
+        cursor.execute("SELECT * FROM `states` WHERE State=%s and Pop=%s", [updateZIP,updatePOP])
         test1 = cursor.rowcount
         if test1 != 1:
-            return updateSTATE + "  failed to update"
+            return updateZIP + "  failed to update"
         else:
-            return 'Population has been updated successfully for State: %s' % updateSTATE
+            return 'Population has been updated successfully for State: %s' % updateZIP
 
 #update webpage
 @app.route('/update',methods = ['POST'])
 def update():
-       user = request.form['ustate']
+       user = request.form['uzip']
        user2 = request.form['upop']
-       return redirect(url_for('updatestatepop', updateSTATE=user, updatePOP=user2))
+       return redirect(url_for('updatezippop', updateZIP=user, updatePOP=user2))
 
 #search page
 @app.route('/search', methods=['GET'])
 def search():
-       user = request.args.get('sstate')
-       return redirect(url_for('searchstate', searchState=user))
+       user = request.args.get('zzip')
+       return redirect(url_for('searchzip', searchZip=user))
 
 
 #root of web server and gots to template (login.html)
